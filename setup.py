a61bbad2a7d9044dda2df742ad2d2be0c26a645f
teegrixx <89885361+Teegrixx@users.noreply.github.com>
2024-04-21 02:22:40 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='google-scraper',
+    version='1.0.0',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'google-scraper=google_scraper.scraper:main'
+        ]
+    },
+    install_requires=[
+        'requests',
+        'beautifulsoup4'
+    ],
+    author='TEEGRIXC',
+    author_email='teegrixx01@gmail.com',
+    description='A simple Google search scraper',
+    url='https://github.com/teegrixx/google-scraper',
+)
